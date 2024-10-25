04f990e946d24be95fd594b2d97e25a8702b7bd4
Aiman-26 <ummeaiman262@gmail.com>
2024-10-25 13:28:11 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,74 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `As a dedicated and eager full-stack developer, I am passionate about learning and building web applications. Although I am just starting my journey, I have gained a strong foundation and honed my skills in front-end technologies like React and back-end technologies like Node.js. I am excited to continue growing my skills and contribute to creating innovative solutions that enhance user experiences and support business objectives.`;
+
+export const ABOUT_TEXT = `As an enthusiastic and adaptable aspiring full-stack developer, I am passionate about building efficient and user-friendly web applications. Though I am just beginning my professional journey, I have developed a solid understanding of technologies like React and Node.js. My interest in web development started from a deep curiosity about how things work, and I am excited to continue learning and growing. I thrive in team settings, enjoy tackling challenges, and aim to contribute to meaningful projects. Outside of coding, I love exploring new technologies and staying engaged with the latest trends in development.
+With a strong commitment to continuous improvement, I am always looking for opportunities to grow both technically and personally. I am confident that my dedication and willingness to learn will make me a valuable contribution to any team. I look forward to contributing fresh ideas and energy to create impactful solutions in the fast-evolving world of web development.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Oct 2023 - Nov 2023",
+    role: "Full Stack Web Development",
+    company: "Take It Smart",
+    description: `Led a project to design a e-commerce platform. Performance Optimization.
+Implemented caching strategies, database
+indexing and reducing page load`,
+    technologies: ["Javascript", "Django", "Html", "CSS", "MySql"],
+  },
+  {
+    year: "Sept 2023 - Oct 2023",
+    role: "Software Engineering Job Simulation",
+    company: "JP Morgan Chase & Co. -Forage",
+    description: `Interface with a stock price data feed. Use JPMorgan chase and Co. frameworks and tools. Display data visuality for traders.Contrubuting to the Open Source Community`,
+    technologies: ["Python3", "Git", "Basic Programming"],
+  },
+  {
+    year: "Oct 2022 - Oct 2022",
+    role: "Python programming & Application Projects",
+    company: "Indoskill",
+    description: `Supported various program activities, assisting with administrative tasks. Assisted with support and execution of virtual engagements. Wrote procedures, reports and technical documents in easy-to-easy
+understand terminology. Implemented strategies to take advantage of new opportunities.`,
+    technologies: ["Python", "Django", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Jal Raksha",
+    image: project1,
+    description:
+      "A centralized platform that hosts peer-reviewed articles on water-saving techniques, case studies, and research findings. Designed and Developed calculators and tools to help users understand their water usage and potential savings. Participated in peer  code reviews to improve code quality and maintainability.  Used tech stack as  HTML,CSS,JS,Figma,Canva,React Native. Managed project version control using GitHub and deployed testing environments.",
+    technologies: ["HTML", "CSS", "JavaScript", "Figma" ,"Canva" ,"React Native", "MongoDB"],
+  },
+  {
+    title: "Codex",
+    image: project2,
+    description:
+      "Designed and developed a scalable, secure decentralized token system on blockchain using Solidity. Led code reviews sessiojs to ensure maintainable architecture and system design. Built on top of a widely adopted blockchain protocol using solidity, ReactJS, MongoDB, HTML, CSS, JS.",
+    technologies: ["Solidity", "ReactJS","MongoDB", "HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information. Managed project version control using GitHub and deployed using vercel. ",
+    technologies: ["React", "Framer Motion", "Tailwind CSS"],
+  },
+  {
+    title: "Web App Clone",
+    image: project4,
+    description:
+      "The Project was based on cloning the Amazon web App using React, HTML, CSS and JavaScript.",
+    technologies: ["React", "HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "34 Ranka Nagar, RT Nagar , Bangalore , India ",
+  phoneNo: "+91 8296224860",
+  email: "ummeaiman262@gmail.com",
+  linkedin: "LinkedIn"
+};
